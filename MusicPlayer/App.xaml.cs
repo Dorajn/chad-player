1f3a07bcacea955b78ccbe904a9514817fd7e830
nier <patrykfulara7@gmail.com>
2025-01-22 12:29:44 +0100

    Add backend drag and drop playlists

--- MusicPlayer/App.xaml.cs
+++ MusicPlayer/App.xaml.cs
@@ -14,7 +14,7 @@ public partial class App : Application
 
         //To powinno byc wywylywane tylko RAZ przy instalacji lub RAZ przez was gdy uruchamiacie aplikacje
         //Ta metoda zapisuje sciezke do rejestru systemu
-        Metadata.SaveMusicFolderPath(@"C:\Users\derqu\Music");
+        Metadata.SaveMusicFolderPath(@"C:\Users\nier\Desktop\Music");
 
         Metadata.absolutePath = Metadata.GetMusicFolderPath();
     }
--- MusicPlayer/MainWindow.xaml.cs
+++ MusicPlayer/MainWindow.xaml.cs
@@ -1,8 +1,8 @@
 using System.Windows;
-using MusicPlayer.Services;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using MusicPlayer.Services;
 using MusicPlayer.ViewModels;
 
 namespace MusicPlayer;
@@ -24,8 +24,7 @@ public partial class MainWindow : Window
         mainViewModel.VolumeSlider_ValueChanged(sender, e, (float)slVolume.Value);
     }
 
-
-    private void ImagePanel_Drop(object sender, DragEventArgs e)
+    private void AudioFiles_Drop(object sender, DragEventArgs e)
     {
         if (e.Data.GetDataPresent(DataFormats.FileDrop))
         {
@@ -34,12 +33,23 @@ public partial class MainWindow : Window
         }
     }
 
+    private void Playlists_Drop(object sender, DragEventArgs e)
+    {
+        if (e.Data.GetDataPresent(DataFormats.FileDrop))
+        {
+            string[] playlistPaths = (string[])e.Data.GetData(DataFormats.FileDrop);
+            Data.AddPlaylists(playlistPaths);
+        }
+    }
+
     private void Button_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
     {
         Button button = sender as Button;
         if (button != null)
         {
-            button.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3d3d3d"));
+            button.Background = new SolidColorBrush(
+                (Color)ColorConverter.ConvertFromString("#3d3d3d")
+            );
         }
     }
 
@@ -48,7 +58,9 @@ public partial class MainWindow : Window
         Button button = sender as Button;
         if (button != null)
         {
-            button.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#1d1d1d"));
+            button.Background = new SolidColorBrush(
+                (Color)ColorConverter.ConvertFromString("#1d1d1d")
+            );
         }
     }
 
@@ -57,7 +69,9 @@ public partial class MainWindow : Window
         Button button = sender as Button;
         if (button != null)
         {
-            button.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#007a13"));
+            button.Background = new SolidColorBrush(
+                (Color)ColorConverter.ConvertFromString("#007a13")
+            );
         }
     }
 
@@ -66,7 +80,9 @@ public partial class MainWindow : Window
         Button button = sender as Button;
         if (button != null)
         {
-            button.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#00e022"));
+            button.Background = new SolidColorBrush(
+                (Color)ColorConverter.ConvertFromString("#00e022")
+            );
         }
     }
 
@@ -78,7 +94,7 @@ public partial class MainWindow : Window
         }
         else
         {
-            SidePanelColumn.Width = new GridLength(350); 
+            SidePanelColumn.Width = new GridLength(350);
         }
 
         isPanelVisible = !isPanelVisible;
@@ -93,7 +109,7 @@ public partial class MainWindow : Window
                 scrollViewer.LineUp();
             else
                 scrollViewer.LineDown();
-            
+
             e.Handled = true;
         }
     }
